2564285c0c7fb16b74211ea8ee69c2eca95c1ab0
diya1677 <diyakathait13@gmail.com>
2025-05-05 00:11:18 +0530

    Add files via upload

---
+++ Q12m5.py
@@ -0,0 +1,7 @@
+import cal1
+print("add",cal1.add1(2,3))
+print("sub",cal1.sub1(5,2))
+print("mul",cal1.mul1(3,4))
+print("div",cal1.div1(10,2))
+print("mod",cal1.mod1(20,2))
+print("pow",cal1.pow1(2,5))
